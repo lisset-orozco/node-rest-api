00a271429ea000e78cf236aa88380a5dca318f21
lisset-orozco <liss612@gmail.com>
2019-06-02 20:53:17 -0500

    GET/:id route

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,6 +3,7 @@ const router = Router();
 
 // Database connection
 import connect from '../database';
+import { ObjectID } from 'mongodb';
 
 router.get('/', async (request, response) => {
   const db = await connect();
@@ -16,6 +17,14 @@ router.post('/', async (request, response) => {
   const task = { title, description }
   const result = await db.collection('tasks').insertOne(task);
   response.json(result.ops[0]);
+});
+
+router.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const db = await connect();
+  const result = await db.collection('tasks').findOne({ _id: ObjectID(id) });
+
+  response.json(result);
 });
 
 export default router;
